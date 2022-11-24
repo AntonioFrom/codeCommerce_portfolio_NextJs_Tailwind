67d726aa7afa8a7acace63202581ca2a22db19d0
38066 <antonguy@ukr.net>
2022-11-24 23:02:53 +0200

    something change)

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import Navbar from '../components/navbar'
+import Navbar from '../components/Navbar'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import About from '../components/About'
 import Contact from '../components/Contact'
 import Main from '../components/Main'
-import Navbar from '../components/navbar'
 import Projects from '../components/Projects'
 import Skills from '../components/Skills'
 
